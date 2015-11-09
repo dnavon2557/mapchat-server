991dda590d781e684c290925b0494b91e676d0a1
Daniel Navon <dnavon2557@gmail.com>
2015-11-09 13:40:40 -0500

    Made it so that lab8 loads without .html at the end

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/cool', function(request,response) {
 	response.send(cool());
 });
 
+app.get('/lab8', function(req, res) {
+	res.send(lab8.html);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
