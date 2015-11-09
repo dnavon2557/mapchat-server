39c52db27ad1c43ffc011a0aded76726109ee6f1
Daniel Navon <dnavon2557@gmail.com>
2015-11-09 13:26:45 -0500

    Updated to actually serve static files from public

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -22,5 +22,6 @@ app.get('/cool', function(request,response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
