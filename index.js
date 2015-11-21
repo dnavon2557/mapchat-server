ba04a4e3ad5dd7743faa49d6f540310cfeff88d4
Daniel Navon <dnavon2557@gmail.com>
2015-11-21 16:18:56 -0500

    Okay CORS dS should really work now because I commented out everything new and CORS was working before

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var cool = require('cool-ascii-faces');
-var allowCrossDomain = function(req, res, next) {
+/*var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'example.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
-});
+});*/
 
 var cors = require('cors');
 var express = require('express');
