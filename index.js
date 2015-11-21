742f09f6b31dae809b40615b9282a9cdb947add4
Daniel Navon <dnavon2557@gmail.com>
2015-11-21 16:03:35 -0500

    Worked on validated logins some. Not sure this will work but attempted query for login based on database containg logins using update()

--- index.js
+++ index.js
@@ -35,27 +35,76 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
+
+var valid_logins = {
+	"login": ['mchow', 'kaytea', 'CindyLytle', 'BenHarris', 'JeremyMaletic', 
+	'LeeMiller', 'EricDapper', 'RichRumfelt', 'VanAmmerman', 'VicJohnson', 
+	'ErinHolleman', 'PatFitzgerald', 'CheriVasquez', 'HarleyRhoden', 
+	'JanetGage', 'HarleyConnell', 'GlendaMaletic', 'JeffSoulen', 'MarkHair', 
+	'RichardDrake', 'CalvinStruthers', 'LeslieDapper', 'JefflynGage', 
+	'PaulRamsey', 'BobPicky', 'RonConnelly', 'FrancieCarmody', 
+	'ColleenSayers', 'TomDapper', 'MatthewKerr', 'RichBiggerstaff', 
+	'MarkHarris', 'JerryRumfelt', 'JoshWright', 'LindyContreras', 
+	'CameronGregory', 'MarkStruthers', 'TravisJohnson', 'RobertHeller', 
+	'CalvinMoseley', 'HawkVasquez', 'LayneDapper', 'HarleyIsdale', 
+	'GaylaSoulen', 'MatthewRichards', 'RoyDuke', 'GaylaRodriquez', 
+	'FrancieGeraghty', 'LisaLytle', 'ErinHair', 'CalvinGraham', 'VanRhoden', 
+	'KeithRumfelt', 'GlendaSmith', 'KathrynJohnson', 'FredVandeVorde', 
+	'SheriMcKelvey', 'RoyMiller', 'PatIsdale', 'JoseRodriquez', 
+	'KelleyRumfelt', 'JanetKinsey', 'RonCampbell', 'BenKerr', 'RobDennison', 
+	'BobOwens', 'CherylLytle', 'LisaSoulen', 'TravisDuke', 'CindyGregory', 
+	'JoyceVandeVorde', 'MatthewScholl', 'RobJohnson', 'EricHawthorn', 
+	'CameronRodriquez', 'JoshRamsey', 'CalvinDuke', 'SheriHeller', 
+	'LeaAmmerman', 'LayneVasquez', 'IMConnell', 'BenHauenstein', 
+	'ColleenKerr', 'HawkRichards', 'LeaIsdale', 'RickSoulen', 'RoyMcFatter', 
+	'KyleContreras', 'MaryHeller', 'KathrynFitzgerald', 'JanetRiedel', 
+	'PatHawthorn', 'KeithHauenstein', 'BenRichards', 'RickVasquez', 
+	'KelleyAmmerman', 'EvanConnelly', 'KendallRumfelt', 'TravisIsdale', 
+	'RobContreras', 'JavierRussell', 'ColleenCampbell', 'JeremyConnelly', 
+	'BenKinsey', 'JanetScholl', 'PaulaLewis', 'LeslieMcFatter', 
+	'MatthewMcAda', 'LeeMuilman', 'KyleMoseley', 'JeffRhoden', 
+	'AnitaHolleman', 'JefflynMcKelvey', 'BobContreras', 'RobFitzgerald', 
+	'BenJohnson']};
+
+
+function update_logins() {
+	db.collection('valid_logins', function (error, coll) {
+		var id = coll.update(
+		{}
+		valid_logins,
+		{upsert: true}
+			);
+	});
+}
+
 app.post('*', function (request, response, next) {
 	var login = request.body.login;
 	var lat = request.body.lat;
 	var lng = request.body.lng;
 	var message = request.body.message;
-	var toInsert = {
-		"login": login,
-		"lat": lat,
-		"lng": lng,
-		"message": message
-	};
-	db.collection('checkins', function(error, coll) {
-		var id = coll.insert(toInsert, function(error, saved) {
-			if (error) {
-				response.send(500);
-			} else{
-				response.status(200);
-				response.send(toInsert);
-			}
+	var errMsg = {"error":"Whoops, something is wrong with your data!"};
+	if (login == db.collection('valid_logins') function(error, coll){
+		var id = coll.find( {'login':login});
+	}) {
+		var toInsert = {
+			"login": login,
+			"lat": lat,
+			"lng": lng,
+			"message": message
+		};
+		db.collection('checkins', function(error, coll) {
+			var id = coll.insert(toInsert, function(error, saved) {
+				if (error) {
+					response.status(500);
+				} else{
+					response.status(200);
+					response.send(toInsert);
+				}
+			});
 		});
-	});
+	} else {
+		response.send(errMsg);
+	}
 });
 
 app.get('/', function(request, response) {
