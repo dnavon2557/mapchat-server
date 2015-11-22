a9d6821a96c49980d77da138cd4011e53e282519
Daniel Navon <dnavon2557@gmail.com>
2015-11-21 19:23:37 -0500

    return all matching elements for lates.json for testing purpose

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/latest.json', function (request, response) {
 	var login = request.body.login;
 	db.collection('checkins', function(error1, coll) {
 		coll.find({"login":login}).sort({"created_at":-1}).toArray( function (error2, data) {
-			response.send(data[0]);
+			response.send(data);
 		});
 	});
 });
